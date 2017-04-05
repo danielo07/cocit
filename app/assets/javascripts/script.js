cc77f557bcbab4521613fcead0447ccaf43c9585
danielo07 <ricardo.daniel0719@gmail.com>
2017-04-04 22:34:38 -0500

    Primera version

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('a').click(function(){
+		$('html, body').stop().animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 500);
+		return false;
+	});
+	AOS.init();
+})
